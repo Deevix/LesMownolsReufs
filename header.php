31abd2c0dcce27eaf68ab38487cd79cebe79d68e
Deevix <97668989+Deevix@users.noreply.github.com>
2022-04-08 10:35:31 +0200

    Update header.php

--- header.php
+++ header.php
@@ -67,10 +67,7 @@
       </ul>
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-          <a class="nav-link" href="login">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="register">Register</a>
+          <a class="nav-link" href="logout.php">Déconnexion</a>
         </li>
         <li class="nav-item">
           <a class="nav-link " href="todo-projet.php">
